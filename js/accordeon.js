c63cd776f22ff3376cf65e96e56f624619e72bcb
timedbase <141355262+timedbase@users.noreply.github.com>
2023-08-13 12:08:39 +0100

    Add files via upload

---
+++ js/accordeon.js
@@ -0,0 +1,115 @@
+
+let accBtn = document.querySelector('.btn-menu');
+let accMenu = document.querySelector('.item-menu');
+
+accBtn.addEventListener('click', function(){
+    accMenu.classList.toggle('opener');
+})
+/*
+let accBtn = document.querySelectorAll(".btn-menu");
+let accMenu = document.querySelectorAll(".item-menu");
+
+
+items.forEach((item) => {
+    item.classList.remove(className);
+})
+
+
+accBtn.forEach((item) => {
+    item.addEventListener('click', () => {
+        accMenu.forEach((items) => {
+            items.classList.toggle('opener')
+        })
+    })
+})*/
+
+let accBtn1 = document.querySelector('.btn-menu1');
+let accMenu1 = document.querySelector('.item-menu1');
+
+accBtn1.addEventListener('click', function(){
+    accMenu1.classList.toggle('opener');
+})
+
+let accBtn2 = document.querySelector('.btn-menu2');
+let accMenu2 = document.querySelector('.item-menu2');
+
+accBtn2.addEventListener('click', function(){
+    accMenu2.classList.toggle('opener');
+})
+
+let accBtn3 = document.querySelector('.btn-menu3');
+let accMenu3 = document.querySelector('.item-menu3');
+
+accBtn3.addEventListener('click', function(){
+    accMenu3.classList.toggle('opener');
+})
+
+let accBtn4 = document.querySelector('.btn-menu4');
+let accMenu4 = document.querySelector('.item-menu4');
+
+accBtn4.addEventListener('click', function(){
+    accMenu4.classList.toggle('opener');
+})
+
+let accBtn5 = document.querySelector('.btn-menu5');
+let accMenu5 = document.querySelector('.item-menu5');
+
+accBtn5.addEventListener('click', function(){
+    accMenu5.classList.toggle('opener');
+})
+
+let accBtn6 = document.querySelector('.btn-menu6');
+let accMenu6 = document.querySelector('.item-menu6');
+
+accBtn6.addEventListener('click', function(){
+    accMenu6.classList.toggle('opener');
+})
+
+let accBtn7 = document.querySelector('.btn-menu7');
+let accMenu7 = document.querySelector('.item-menu7');
+
+accBtn7.addEventListener('click', function(){
+    accMenu7.classList.toggle('opener');
+})
+
+let accBtn8 = document.querySelector('.btn-menu8');
+let accMenu8 = document.querySelector('.item-menu8');
+
+accBtn8.addEventListener('click', function(){
+    accMenu8.classList.toggle('opener');
+})
+
+let accBtn9 = document.querySelector('.btn-menu9');
+let accMenu9 = document.querySelector('.item-menu9');
+
+accBtn9.addEventListener('click', function(){
+    accMenu9.classList.toggle('opener');
+})
+
+let accBtn10 = document.querySelector('.btn-menu10');
+let accMenu10 = document.querySelector('.item-menu10');
+
+accBtn10.addEventListener('click', function(){
+    accMenu10.classList.toggle('opener');
+})
+
+let accBtn11 = document.querySelector('.btn-menu11');
+let accMenu11 = document.querySelector('.item-menu11');
+
+accBtn11.addEventListener('click', function(){
+    accMenu11.classList.toggle('opener');
+})
+
+let accBtn12 = document.querySelector('.btn-menu12');
+let accMenu12 = document.querySelector('.item-menu12');
+
+accBtn12.addEventListener('click', function(){
+    accMenu12.classList.toggle('opener');
+})
+
+let accBtn13 = document.querySelector('.btn-menu13');
+let accMenu13 = document.querySelector('.item-menu13');
+
+accBtn13.addEventListener('click', function(){
+    accMenu13.classList.toggle('opener');
+})
---
+++ js/script.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // конечная дата, например 1 июля 2021
+    const deadline = new Date(new Date().getFullYear(), new Date().getMonth() + 1, 01);
+    // id таймера
+    let timerId = null;
+    // склонение числительных
+    function declensionNum(num, words) {
+      return words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
+    }
+    // вычисляем разницу дат и устанавливаем оставшееся времени в качестве содержимого элементов
+    function countdownTimer() {
+      const diff = deadline - new Date();
+      if (diff <= 0) {
+        clearInterval(timerId);
+      }
+      const days = diff > 0 ? Math.floor(diff / 1000 / 60 / 60 / 24) : 0;
+      const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
+      const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
+      const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
+      $days.textContent = days < 10 ? '0' + days : days;
+      $hours.textContent = hours < 10 ? '0' + hours : hours;
+      $minutes.textContent = minutes < 10 ? '0' + minutes : minutes;
+      $seconds.textContent = seconds < 10 ? '0' + seconds : seconds;
+      $days.dataset.title = declensionNum(days, ['days', 'days', 'days']);
+      $hours.dataset.title = declensionNum(hours, ['hours', 'hours', 'hours']);
+      $minutes.dataset.title = declensionNum(minutes, ['minutes', 'minutes', 'minutes']);
+      $seconds.dataset.title = declensionNum(seconds, ['seconds', 'seconds', 'seconds']);
+    }
+    // получаем элементы, содержащие компоненты даты
+    const $days = document.querySelector('.timer__days');
+    const $hours = document.querySelector('.timer__hours');
+    const $minutes = document.querySelector('.timer__minutes');
+    const $seconds = document.querySelector('.timer__seconds');
+    // вызываем функцию countdownTimer
+    countdownTimer();
+    // вызываем функцию countdownTimer каждую секунду
+    timerId = setInterval(countdownTimer, 1000);
+  });
